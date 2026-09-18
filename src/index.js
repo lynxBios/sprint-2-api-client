ca91024afceeb31d0224c1fd8fccffb82c5f83e6
Radzkovich Tatsiana <lregvjlke@gmail.com>
2026-09-18 22:34:06 +0200

    feat: add parallel user requests and assertions

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { ReqResClient } from './ReqResClient.js';
+import assert from 'node:assert';
+
+const USER_IDS = [1, 2, 3];
+
+const client = new ReqResClient();
+
+const requests = USER_IDS.map((id) => {
+  return client.getUser(id);
+});
+
+const requestedUsersList = await Promise.all(requests);
+
+const usersEmail = requestedUsersList.map((user) => {
+  return user.data.email;
+});
+
+assert.ok(Array.isArray(usersEmail));
+assert.strictEqual(usersEmail.length, 3);
