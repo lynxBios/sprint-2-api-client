002c20e1da81dada8b263e79b297dcb00c7fac52
Radzkovich Tatsiana <lregvjlke@gmail.com>
2026-09-18 09:29:37 +0200

    feat: add request handling

--- src/ReqResClient.js
+++ src/ReqResClient.js
@@ -13,4 +13,17 @@ export class ReqResClient {
     this.baseUrl = process.env.API_BASE_URL;
     this.apiKey = process.env.REQRES_API_KEY;
   }
+
+  async _request(endpoint, options) {
+    const url = this.baseUrl.concat(endpoint);
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      const err = new Error(`HTTP ${response.status}`);
+      err.status = response.status;
+      throw err;
+    }
+
+    return response.json();
+  }
 }
